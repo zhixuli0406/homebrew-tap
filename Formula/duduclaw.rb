789d3d05b57d545681673bc3511bff805b729907
zhixu li <zhixuli0406@gmail.com>
2026-03-15 22:15:01 +0800

    chore: bump to v0.1.4

--- Formula/duduclaw.rb
+++ Formula/duduclaw.rb
@@ -1,8 +1,8 @@
 class Duduclaw < Formula
   desc "Multi-Agent AI Assistant Platform"
   homepage "https://github.com/zhixuli0406/DuDuClaw"
-  url "https://github.com/zhixuli0406/DuDuClaw.git", tag: "v0.1.3"
-  version "0.1.3"
+  url "https://github.com/zhixuli0406/DuDuClaw.git", tag: "v0.1.4"
+  version "0.1.4"
   license "MIT"
 
   head "https://github.com/zhixuli0406/DuDuClaw.git", branch: "main"
