fdd480eeb7e2964d938e3af0c75b0b3042fc6da5
zhixu li <zhixuli0406@gmail.com>
2026-03-28 14:21:52 +0800

    fix: update SHA256 for v0.7.0 release binary

--- Formula/duduclaw.rb
+++ Formula/duduclaw.rb
@@ -5,7 +5,7 @@ class Duduclaw < Formula
   license "MIT"
 
   url "https://github.com/zhixuli0406/DuDuClaw/releases/download/v0.7.0/duduclaw-v0.7.0-aarch64-apple-darwin.tar.gz"
-  sha256 "UPDATE_SHA256_AFTER_RELEASE_BUILD"
+  sha256 "6ce3e9f9e1fd4a1a98d9926cf74e29188ab01fed75a1d9cdcf13c5d9b3388999"
 
   depends_on :macos
   depends_on "python@3.12" => :recommended
