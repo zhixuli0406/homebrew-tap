b7b8cce06bde754e6797c81ff661cc0dbef2bb31
zhixu li <zhixuli0406@gmail.com>
2026-04-16 22:55:09 +0800

    fix(duduclaw-pro): use public release repo URL + correct sha256 for 1.4.31

--- Formula/duduclaw-pro.rb
+++ Formula/duduclaw-pro.rb
@@ -6,11 +6,11 @@ class DuduclawPro < Formula
 
   # Pre-built tarball from public release repo (binary + Python SDK)
   if Hardware::CPU.arm?
-    url "https://github.com/zhixuli0406/duduclaw-pro/releases/download/v#{version}/duduclaw-pro-aarch64-apple-darwin.tar.gz"
-    sha256 "ec56ef62909aa15e39ecfb6de9b218108891df02654004b3121c115a60d4389e"
+    url "https://github.com/zhixuli0406/duduclaw-pro-releases/releases/download/v#{version}/duduclaw-pro-aarch64-apple-darwin.tar.gz"
+    sha256 "f2fe37f0d90808a812984f1be51fab09b3a20fa95f14e2fde2ef2e80653d1554"
   else
-    url "https://github.com/zhixuli0406/duduclaw-pro/releases/download/v#{version}/duduclaw-pro-x86_64-apple-darwin.tar.gz"
-    sha256 "9995c6346134244c963a10cd49c57e5a07d5fad163f34b0f0d7f6c819792ebcc"
+    url "https://github.com/zhixuli0406/duduclaw-pro-releases/releases/download/v#{version}/duduclaw-pro-x86_64-apple-darwin.tar.gz"
+    sha256 "1adc8c99ce2e5fd1856f9eebf757a96ef0c6cc140d2774f3a48375d5d71a024e"
   end
 
   depends_on :macos
